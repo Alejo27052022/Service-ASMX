66a71611d2a9922b51e9a8b2b31c09332fbd8be0
Stalin <stalinalejandro19@outlook.es>
2023-08-07 18:48:57 -0500

    Add Project ClienteAsmx in using Service SOAP

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ClienteAsmx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCall = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnCall
+            // 
+            this.BtnCall.Location = new System.Drawing.Point(316, 167);
+            this.BtnCall.Name = "BtnCall";
+            this.BtnCall.Size = new System.Drawing.Size(149, 79);
+            this.BtnCall.TabIndex = 0;
+            this.BtnCall.Text = "Llamar";
+            this.BtnCall.UseVisualStyleBackColor = true;
+            this.BtnCall.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnCall);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCall;
+    }
+}
+
